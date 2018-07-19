c6477d66d08c201d4f0055cbb370ba4fdcb1fde9
jartigag <jartigag@protonmail.com>
2018-07-19 14:24:18 +0200

    retrieveData v0.4, genDict v0.4

--- genDict.py
+++ genDict.py
@@ -1,44 +1,80 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#author: Javier Artiga Garijo (v0.3)
+#author: Javier Artiga Garijo (v0.4)
 #author: Aruna Prem Bianzino (v0.1, typosquattinggeneration.py)
-#date: 22/06/2018
-#version: 0.3
+#date: 03/07/2018
+#version: 0.4 (it accepts arguments)
 #GENerate a DICTionary of DOMAINS from a list of Official Domains and a list of ccTLDs
 
+#usage: genDict.py [-v] tldsJSONFile domainsDirectory outputDictFile
+
+import argparse
 import os
-import subprocess
 import json
 
-files = []
-doms = []
-tlds = json.load(open('originalTLDs.dat'))
-res = []
+def genDict(tldsFile,domainsDir,verbose):
+	files = []
+	doms = []
+	result = [] # array with all domains for all clients, as ["customer":cust_code,"domains":res]
+	tlds = json.load(open(tldsFile))
 
-for f in os.listdir(os.getcwd()+'/DAT'):
-	files.append(f)
-for file in files:
-	with open('DAT/'+file) as f:
-		doms.append(f.read().splitlines())
+	for f in os.listdir(domainsDir):
+		files.append(f)
+	for file in files:
+		with open(domainsDir+file) as f:
+			doms.append(f.read().splitlines())
 
-i=0
-
-for customer in files:
-	i+=1
-	#print("customer %i: %s" % (i,customer))
-	for d in doms:
-		#print("		%i official domains: %s" % (len(d),d))
+	i=0
+	ndoms=0
+	ncombs=0
+	for c in files:
+		res = [] # array with domains combinations for a client
+		e = {} # element (type: dictionary) to append in result array
+		cust_code = c.split('_-_')[0] # customer code
+		i+=1
+		d=doms[i-1]
+		if verbose:
+			print("%i - %s 	%i doms (%i combs, " % (i,cust_code,len(d),len(d)*len(tlds)),end="")
+		ndoms+=len(d)
+		ncombs+=len(d)*len(tlds)
 		for url in d:
-			u = url.rsplit('.',1)[0].lower()
-			#generate combinations
+			u = url.rsplit('.',1)[0].lower() # name of the domain
+			#generate combinations with the name and the tlds:
 			for tld in tlds:
 				res.append(u+tld)
-				'''
-				#sort and save combinations in file
-				command = 'cat temp.aux | grep -v "xn--" | sort -u >> "combs-' + customer + '.txt"'
-				os.system(command)
-				'''
-# REMOVE DUPlicates in res and SORT alphabetically:				
-res = sorted(list(set(res)))
-for r in res:
-	print(r)
+		res = list(set(res)) # REMOVE DUPlicates in res
+		e['customer'] = cust_code
+		e['domains'] = res
+		result.append(e)
+
+	if verbose:
+		print("TOTAL domains:",ndoms)
+		print("TOTAL combinations (with duplicates):",ncombs)
+		print("removing duplicates")
+		c=0
+		for r in result:
+			c+=len(r['domains'])
+		print("TOTAL COMBINATIONS:",c,"(%i duplicates domains removed)"%(ncombs-c))
+
+	return result
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('tldsJSONFile',help='e.g.: ccTLDS.json')
+	parser.add_argument('domainsDirectory',help='e.g.: files/DAT/')
+	parser.add_argument('outputDictFile',help='e.g.: dict-37tlds.json')
+	parser.add_argument('-v','--verbose',action='store_true')
+	args = parser.parse_args()
+
+	results = genDict(args.tldsJSONFile,args.domainsDirectory,args.verbose)
+
+	# print results as a json to outputDictFile
+	with open(args.outputDictFile,'w') as f:
+		print("[",end="",file=f)
+		for r in results:
+			if not results.index(r)==len(results)-1:
+				print(json.dumps(r, indent=2, sort_keys=True),end=",\n",file=f)
+			else:
+				print(json.dumps(r, indent=2, sort_keys=True),end="",file=f)
+		print("]",file=f)
--- retrieveData.py
+++ retrieveData.py
@@ -1,116 +1,194 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#author: Javier Artiga Garijo (v0.2)
-#date: 25/06/2018
-#version: 0.2 (based on the draft script dnsCheck.py)
-#for a dictionary of domains, RETRIEVE DATA of whois (status, owner, registration_date, owner_change, creation_date)
-#and get their ip, mx records and if domain/subdomains returns something.
+#author: Javier Artiga Garijo (v0.4)
+#date: 13/07/2018 (some improvements)
+#version: 0.4 (based on ts-updater.py)
+#given a dictionary of domains, RETRIEVE DATA of whois, ip, mx records, webs for each domain
+#and classify it as low/high priority + status info.
 #results of each domain are stored in an array of Domain objects with all their collected info.
+#
+#recommended execution: /usr/bin/time -o time.txt python3 retrieveData.py [-v] dictFile.json outputFile.json >> logFile.log
 
-#TODO: Creation Date, Updated Date could be took from whois
-#TODO: all .web in results are ['False']..
-
-#recommended execution: /usr/bin/time -o time.txt python3 retrieveData.py dict.txt output.dat
+#TO-DO LIST (18/07/2018)
+#TODO: threads until block
+#TODO: try-except to control when we are blocked
+#FIXME: 2596 status (52,2%) stay as 'resolving'
 
 import argparse
 from datetime import date, timedelta, datetime
+from time import time
 import whois
 import dns.resolver
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import socket
 import json
+#from dnstwist import DomainFuzz
 
 class Domain:
     def __init__(self):
         self.status = 'not resolving'
         self.owner = ''
-        self.reg_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        self.owner_change = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        self.creation_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        self.ip = ''
+        self.reg_date = convertDatetime(datetime.now())
+        self.owner_change = convertDatetime(datetime.now())
+        self.creation_date = convertDatetime(datetime.now())
+        self.ip = []
         self.mx = []
-        self.web = []
+        self.web = [] # http reqs
+        self.webs = [] # https reqs
         self.domain = ''
-        self.subdomains = '' #TODO
-        self.test_freq = '' #TODO
-        self.generation = '' #TODO (venía dado en dictionary.txt)
-        self.customer = '' #TODO (venía dado en dictionary.txt)
-        self.priority = '' #TODO
-        self.timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.subdomains = '' #TODO (vacíos hasta fase2)
+        self.test_freq = ''
+        self.generation = '' #TODO (vacíos hasta fase2)
+        self.customer = ''
+        self.priority = ''
+        self.timestamp = convertDatetime(datetime.now())
 
-def retrieveDomainsDataFromFile(file):
+def convertDatetime(date):
+    if isinstance(date, datetime): #if argument's type is datetime
+        return date.strftime('%Y-%m-%d %H:%M:%S')
+    elif isinstance(date, str): #if argument's type is string
+        return datetime.strptime(date,'%Y-%m-%d %H:%M:%S')
+    else:
+        return False
+
+def retrieveDomainsDataFromFile(dictFile,verbose):
     results = []
-    with open(file) as f:
-            domains = f.read().splitlines()
-    for dom in domains:
-        d = Domain()
-        d.domain = dom
+    data = json.load(open(dictFile))
+    #data = data[0:1] ## PARA PRUEBA CORTA
+    for e in data:
+        for dom in e['domains']:
+            d = Domain()
+            d.domain = dom
+            d.customer = e['customer']
 
-        # check whois
-        try:
-            w = whois.query(dom)
-            d.status = 'resolving'
-            d.owner = w.name
-            d.reg_date = w.last_updated.strftime('%Y-%m-%d %H:%M:%S')
-            d.owner_change = w.creation_date.strftime('%Y-%m-%d %H:%M:%S')
-            d.creation_date = w.creation_date.strftime('%Y-%m-%d %H:%M:%S')
-        except:
-            #TODO: sometimes it fails because whois parses the result wrongly (ej: "ValueError: Unknown date format: 'registrar: the registrar, s.l.u'")
-            pass
+            start_time = time()
+            check_whois(d)
+            get_ip(d)
+            get_mx(d)
+            check_web(d)
+            #check_subomains(d)
+            end_time = time()
 
-        # get ip
-        try:
-            d.ip = str(socket.gethostbyname(dom))
-        except:
-            pass
+            if verbose:
+                print("cust%i - [%i/%i]"%(data.index(e)+1,e['domains'].index(dom)+1,len(e['domains'])),
+                    "[-]" if d.ip==[] else "[x]",
+                    "%s %s"%(d.customer,d.domain),
+                    "(%.2f secs)"%(end_time-start_time))
 
-        # check if there are mx records
-        try:
-            d.mx = []
-            ans = dns.resolver.query(dom, 'MX')
-            for rdata in ans:
-                d.mx.append(str(rdata.exchange))
-        except:
-            pass
+            results.append(d)
 
-        # check if the url returns something
-        '''
+    return results
+
+def check_whois(d):
+    # CHECK WHOIS
+    try:
+        w = whois.query(d.domain)
+        d.status = 'resolving' #FIXME: 2596 status (52,2%) stay as 'resolving', instead of 'to be verified' or 'parked'
+        d.owner = w.name
+        d.reg_date = convertDatetime(w.last_updated)
+        d.owner_change = convertDatetime(w.last_updated)
+        d.creation_date = convertDatetime(w.creation_date)
+        # ASSIGN PRIORITY
+        #domain resolves:
+        if convertDatetime(d.reg_date)==False:
+            pass #to discard wrong reg_dates
+        elif convertDatetime(d.reg_date)+timedelta(days=7) < datetime.now():
+            #registered less than 1 week ago:
+            d.priority = 'high'
+            d.status = 'to be verified'
+        else:
+            #registered more than 1 week ago:
+            d.priority = 'low'
+            d.status = 'parked'
+    except:
+        #TODO: sometimes it fails because whois parses the result wrongly
+        #(ej: "ValueError: Unknown date format: 'registrar: the registrar, s.l.u'")
+        #domain doesn't resolve:
+        if datetime.now()-timedelta(seconds=200) <= convertDatetime(d.reg_date):
+            #not registered (because reg_date is now, with a margin of 200 secs):
+            d.priority = 'low'
+            d.status = 'very low priority'
+        elif convertDatetime(d.reg_date) > datetime.now()-timedelta(days=7):
+            #registered less than 1 week ago:
+            d.priority = 'high'
+            d.status = 'very suspicious'
+        else:
+            #registered more than 1 week ago:
+            d.priority = 'high'
+            d.status = 'suspicious'
+    # ASSIGN TEST_FREQ
+    if d.priority=='high':
+        d.test_freq = '1'
+    elif d.priority=='low':
+        d.test_freq = '14'
+
+def get_ip(d):
+    # GET IP
+    try:
+        d.ip.append(str(socket.gethostbyname(d.domain)))
+    except:
+        pass
+
+def get_mx(d):
+    # GET MX RECORDS
+    try:
+        ans = dns.resolver.query(d.domain, 'MX')
+        for rdata in sorted(ans):
+            d.mx.append(str(rdata.exchange))
+    except:
+        pass
+
+def check_web(d):
+    # CHECK WEB
+    try:
+        requests.get('http://' + dom)
+        d.web.append(True)
+    except:
+        d.web.append(False)
+    try:
+        # in order to ignore "InsecureRequestWarning: Unverified HTTPS request is being made.":
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        requests.get('https://' + dom, verify=False)
+        d.webs.append(True)
+    except:
+        d.webs.append(False)
+
+def check_subdomains(d):
+    # CHECK SUBDOMAINS
+    '''
+    #TODO: (fase2) generate subdomains with dnstwist:
+    subdoms = []
+    n=0
+    for do in res:
+        d = DomainFuzz(do)
+        d.generate()
+        for m in d.domains:
+            if 'Subdomain' in m['fuzzer']:
+                subdoms.append(m['domain-name'])
+                n+=1
+    if verbose:
+        print("%i subdomains)" % (n))
+    for s in subdoms:
+        res.append(s)
+    for sub in subdomains:
         try:
-            requests.get('https://' + dom, verify=False)
-            #TODO: ignore "InsecureRequestWarning: Unverified HTTPS request is being made."
-            #       it stops when d.web = False
+            requests.get('http://' + sub)
             web_bool = 'True'
         except:
             web_bool = 'False'
-        d.web.append(web_bool)
-        '''
-
-        # check if the knowns subdomains return something
-        '''
-        for sub in subdomains:
-            try:
-                requests.get('http://' + sub)
-                web_bool = 'True'
-            except:
-                web_bool = 'False'
-            web_array.append(web_bool)
-        '''
-
-        results.append(d)
-        #print("[%i/%i]"%(domains.index(dom),len(domains)),
-        #    "\033[1m","[-]" if d.status=='not resolving' else "[x]","%s:\033[0m"%(d.domain),
-        #    d.__dict__) #DEBUGGING
-
-    return results
+        web_array.append(web_bool)
+    '''
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('dictFile')
-    parser.add_argument('outputFile')
+    parser.add_argument('dictFile',help='e.g.: dict-37tlds.json')
+    parser.add_argument('outputFile',help='e.g.: output-37tlds.json')
+    parser.add_argument('-v','--verbose',action='store_true')
     args = parser.parse_args()
 
-    results = retrieveDomainsDataFromFile(args.dictFile)
+    results = retrieveDomainsDataFromFile(args.dictFile,args.verbose)
 
     # print results as a json (an array of jsons, actually) to outputFile
     with open(args.outputFile,'w') as f:
