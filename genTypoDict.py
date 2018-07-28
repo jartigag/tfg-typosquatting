2f43712a783d663dce218b0d14eaf991dfb8fcee
jartigag <jartigag@protonmail.com>
2018-07-28 15:38:00 +0200

    hits stats in genTypoDict

--- genTypoDict.py
+++ genTypoDict.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#author: Javier Artiga Garijo (v0.2)
-#date: 27/07/2018
-#version: 0.2 (allows technics choice)
+#author: Javier Artiga Garijo (v0.3)
+#date: 28/07/2018
+#version: 0.3 (registered stats)
 # GENerate a DICTionary of DOMAINS from a list of Official Domains and a list of ccTLDs,
 # including TYPOsquatting variations
 
 #usage: genTypoDict.py [-v] tldsJSONFile domainsDirectory outputDictFile [-t technic1 technic2]
 
-#TODO: add stats
 #TODO: REMOVE DUPlicates in res
+#TODO?: adapt generate_json to create a dict of registered domains only
 
 import argparse
 import os
 import json
-from dnstwist import DomainFuzz, generate_json
+from dnstwist import DomainFuzz, DomainThread, generate_json
+import queue
+import time
 
 def genDict(tldsFile,domainsDir,verbose,technics):
 	files = []
 	doms = []
 	result = [] # array with all domains for all clients, as ["customer":cust_code,"domains":res]
+	registered = [] # array with all registered domains for all clients, as ["customer":cust_code,"domains":reg]
 	tlds = json.load(open(tldsFile))
 
 	for f in os.listdir(domainsDir):
@@ -34,7 +37,9 @@ def genDict(tldsFile,domainsDir,verbose,technics):
 	nvars=0
 	for c in files:
 		res = [] # array with domains combinations for a client
+		reg = [] # array with registered domains for a client
 		e = {} # element (type: dictionary) to append in result array
+		r = {} # element (type: dictionary) to append in registered array
 		cust_code = c.split('_-_')[0] # customer code
 		i+=1
 		d=doms[i-1]
@@ -72,19 +77,27 @@ def genDict(tldsFile,domainsDir,verbose,technics):
 					dfuzz.generate_transposition()
 				if 'vowelswap'in args.technics:
 					dfuzz.generate_vowelswap()
-				res.append(dfuzz.domains)
+				res.extend(dfuzz.domains)
 				nvars += len(dfuzz.domains)
+				#the same as result, but with registered domains:
+				reg.extend(procRegs(dfuzz.domains))
 		if verbose:
-			print(" - %i variations in total"%(len(dfuzz.domains)*len(res)))
+			print(" - %i variations in total	"%(len(res)),end="")
+			hits_total = len(reg)
+			hits_percent = 100 * hits_total / len(res)
+			print(' %d hits (%d%%)' % (hits_total, hits_percent))
 		#res = list(set(res)) #TODO: REMOVE DUPlicates in res
 		e['customer'] = cust_code
 		e['domains'] = res
 		result.append(e)
+		r['customer'] = cust_code
+		r['domains'] = reg
+		registered.append(r)
 
 	if verbose:
-		print("TOTAL domains:",ndoms)
+		print("TOTAL official domains:",ndoms)
 		print("TOTAL combinations: %i (using %i tlds)"%(ndoms*len(tlds),len(tlds)))
-		print("TOTAL variations:",nvars)
+		print("TOTAL variations:",nvars,"(x%.2f domain combinations)"%(nvars/(ndoms*len(tlds))))
 		# print("removing duplicates")
 		# c=0
 		# for r in result:
@@ -93,10 +106,35 @@ def genDict(tldsFile,domainsDir,verbose,technics):
 
 	return result
 
+def procRegs(domains):
+	regCustomer = []
+	jobs = queue.Queue()
+	global threads
+	threads = []
+	for i in range(len(domains)):
+		jobs.put(domains[i])
+	for i in range(4): #param: 4 threads
+		worker = DomainThread(jobs)
+		worker.setDaemon(True)
+		worker.option_whois = True
+		worker.start()
+		threads.append(worker)
+	while not jobs.empty():
+		time.sleep(10**-9) #to prevent "RuntimeError: can't start new thread" or "Too much open files"
+	for worker in threads:
+		worker.stop()
+	for d in domains:
+		if 'dns-ns' in d or 'dns-a' in d:
+			r = {}
+			r['fuzzer'] = d['fuzzer']
+			r['domain-name'] = d['domain-name']
+			regCustomer.append(r)
+	return regCustomer
+
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('tldsJSONFile',help='e.g.: files/tlds-44-all.json')
+	parser.add_argument('tldsJSONFile',help='e.g.: files/tlds-44.json')
 	parser.add_argument('domainsDirectory',help='e.g.: files/DAT/')
 	parser.add_argument('outputDictFile',help='e.g.: dict-44tlds.json')
 	parser.add_argument('-v','--verbose',action='store_true')
--- genTypoDicts.sh
+++ genTypoDicts.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 #author: Javier Artiga Garijo (v0.1)
-#date: 27/07/2018
+#date: 28/07/2018
 #version: 0.1
 #
 # execute genTypoDict.py with all the possible parameters
-# usage: bash genTypoDicts.sh technic
+# usage: bash genTypoDicts.sh
 
-technic=$1
-for n in 3 7 44 #number of tlds
+#for n in 3 7 44 #number of tlds
+$n=44
+for technic in omission repetition addition
 do
 	/usr/bin/time -o times/time-dict-typo$n-tlds-$technic.txt \
 		python3 genTypoDict.py -v files/tlds-$n.json files/DAT/ dicts/dict-typo$n-tlds-$technic.json -t $technic \
