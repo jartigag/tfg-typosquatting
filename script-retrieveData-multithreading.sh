b37dcb23f36770e8d3806bd5c1bf9a53afcf0129
jartigag <jartigag@protonmail.com>
2018-08-10 01:37:11 +0200

    retrieveData with bash multithreading

---
+++ script-retrieveData-multithreading.sh
@@ -0,0 +1,3 @@
+dicts=typoDicts/*
+mkdir output-typoDicts; mkdir time-typoDicts
+for dict in $dicts ; do echo "processing $dict.."; /usr/bin/time -o time-$dict.txt python3 retrieveData.py -d $dict -o output-$dict & done
