e37442e5f5cb13ce52b1afe91688f9a10523cfaf
jartigag <jartigag@protonmail.com>
2018-09-11 11:42:18 +0200

    corrections for crontab

--- STAGE1/multiUpDat.sh
+++ STAGE1/multiUpDat.sh
@@ -6,21 +6,24 @@
 #
 # execute updateData.py on multiple processes (one for each customer)
 # usage: bash multiUpDat.sh ESindex
+#
+# crontab: 0 2,10,18 * * * date >> ~/STAGE1/log-multiUpDat.sh
+# crontab: 0 2,10,18 * * * bash ~/STAGE1/multiUpDat.sh data >> ~/STAGE1/log-multiUpDat.sh
 
 echo "$(date) - running script.."
 # custList = []
 # for c in os.listdir(customersDomainsDirectory):
 #       custList.append(c.split('_-_')[0]) # customer code
 
-arrFiles=(files/DAT/*)
+arrFiles=(~/STAGE1/files/DAT/*)
 #for filename in ${arrFiles[@]:0:3}
 for filename in ${arrFiles[@]}
 do
         name=$(basename $filename)
         cust=${name%_-_*} #cust=filename.split('_-_')[0]
-        /usr/bin/time -o times/time-upDat-$cust.txt \
-                python3 updateData.py $cust $1 -v \
-                >> logs/logs-upDat/log-$cust.log #&
+        /usr/bin/time -o ~/STAGE1/times/time-upDat-$cust.txt \
+                python3 ~/STAGE1/updateData.py $cust $1 -v \
+                >> ~/STAGE1/logs/logs-upDat/log-$cust.log #&
         echo "$(date) - $cust's data updated"
 done
 #ps -AF | grep python3
